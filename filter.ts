d11bfe47f88daa37cc3c9329d8bc1e422c29473a
eclipseop <mh@otterdev.com>
2022-01-16 10:16:16 -0600

    eslint stuff

--- filter.ts
+++ filter.ts
@@ -1,86 +1,84 @@
-import { State } from ".";
+import { State } from '.';
 
 const arraysEqual = (a: any[], b: any[]) => {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length != b.length) return false;
-    a.sort();
-    b.sort();
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+  a.sort();
+  b.sort();
 
-    for (let i = 0; i < a.length; ++i) {
-        if (a[i] !== b[i]) return false;
-    }
-    return true;
-}
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+};
 
 export const filterOnlyWordsWithGuaranteedLetters = (wordList: string[], state: State[]): string[] => {
     
-    const wordMustContain: string[] = state.filter(s => s.contains).map(s => s.letter);
+  const wordMustContain: string[] = state.filter(s => s.contains).map(s => s.letter);
 
-    return wordList.filter(word => {
-        const letters = word.split('');
+  return wordList.filter(word => {
+    const letters = word.split('');
 
-        const availableLettersInWord = [...new Set(
-            letters.filter(l => wordMustContain.includes(l))
-        )];
+    const availableLettersInWord = [...new Set(
+      letters.filter(l => wordMustContain.includes(l))
+    )];
 
-
-        return arraysEqual(availableLettersInWord, wordMustContain)
-    })
-}
+    return arraysEqual(availableLettersInWord, wordMustContain);
+  });
+};
 
 // Bad letters are letters we've guessed and we got an answer of no.
 export const filterOutBadLetters = (wordList: string[], state: State[]): string[] => {
-    const availableLetters: string[] = state.filter(s => !s.checked || s.contains).map(s => s.letter);
-    //console.log(availableLetters)
+  const availableLetters: string[] = state.filter(s => !s.checked || s.contains).map(s => s.letter);
 
-    return wordList.filter(word => {
-        const letters = word.split('');
+  return wordList.filter(word => {
+    const letters = word.split('');
 
-        for (const letter of letters) {
-            if (!availableLetters.includes(letter)) return false;
-        }
-        //console.log(word)
-        return true;
-    })
-}
+    for (const letter of letters) {
+      if (!availableLetters.includes(letter)) return false;
+    }
+
+    return true;
+  });
+};
 
 export const filterOutGuessedWords = (wordList: string[], guessedWords: string[]): string[] => {
-    return wordList.filter(word => {
-        return !guessedWords.includes(word);
-    })
-}
+  return wordList.filter(word => {
+    return !guessedWords.includes(word);
+  });
+};
 
 export const filterOnlyAbsolutePosition = (wordList: string[], guaranteedLetters: string[]): string[] => {
-    return wordList.filter(word => {
-        for (let i = 0; i < word.length; i++) {
-            const gLetter = guaranteedLetters[i];
-            const guessLetter = word[i];
+  return wordList.filter(word => {
+    for (let i = 0; i < word.length; i++) {
+      const gLetter = guaranteedLetters[i];
+      const guessLetter = word[i];
 
-            if (gLetter === '') continue;
+      if (gLetter === '') continue;
 
-            if (gLetter !== guessLetter) {
-                return false;
-            }
-        }
-        return true;
-    })
-}
+      if (gLetter !== guessLetter) {
+        return false;
+      }
+    }
+    return true;
+  });
+};
 
 export const filterOutWrongPositionLetters = (wordList: string[], state: State[], guaranteedLetters: string[]): string[] => {
-    const wrongPositionLetters = state.filter(s => s.checkedPosition && s.checkedPosition.length > 0);
+  const wrongPositionLetters = state.filter(s => s.checkedPosition && s.checkedPosition.length > 0);
     
-    return wordList.filter(word => {
-        for (let i = 0; i < word.length; i++) {
-            const letter = word[i];
+  return wordList.filter(word => {
+    for (let i = 0; i < word.length; i++) {
+      const letter = word[i];
 
-            if (guaranteedLetters[i] === letter) continue;
+      if (guaranteedLetters[i] === letter) continue;
             
-            const previousPositions = wrongPositionLetters.find(s => s.letter === letter);
-            if (previousPositions && previousPositions.checkedPosition?.includes(i)) {
-                return false;
-            }
-        }
-        return true;
-    })
-}
+      const previousPositions = wrongPositionLetters.find(s => s.letter === letter);
+      if (previousPositions && previousPositions.checkedPosition?.includes(i)) {
+        return false;
+      }
+    }
+    return true;
+  });
+};
--- index.ts
+++ index.ts
@@ -4,16 +4,16 @@ import * as readLine from 'readline';
 import { filterOnlyAbsolutePosition, filterOnlyWordsWithGuaranteedLetters, filterOutBadLetters, filterOutGuessedWords, filterOutWrongPositionLetters } from './filter';
 
 const rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 const getWordList = async (): Promise<string[]> => {
-    const wordList = await fsPromises.readFile('./words.txt', 'utf8');
+  const wordList = await fsPromises.readFile('./words.txt', 'utf8');
     
-    const words = wordList.split('\r\n');
-    return words;
-}
+  const words = wordList.split('\r\n');
+  return words;
+};
 
 export type State = {
     letter: string,
@@ -22,73 +22,69 @@ export type State = {
     checkedPosition: number[] | undefined,
 }
 
-const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-
-let letters: State[] = alphabet.map(letter => ({
-    letter,
-    checked: false,
-    contains: false,
-    checkedPosition: undefined,
+const letters: State[] = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => ({
+  letter,
+  checked: false,
+  contains: false,
+  checkedPosition: undefined,
 }));
 
-let FOR_SURE = ['','','','',''];
+const FOR_SURE = ['','','','',''];
 
 const gussedWords: string[] = [];
 
 const generateGuess = async (currentState: State[]): Promise<string> => {
-    const availableLetters: string[] = currentState.filter(state => !state.checked || state.contains).map(state => state.letter);
+  const availableLetters: string[] = currentState.filter(state => !state.checked || state.contains).map(state => state.letter);
 
-    if (availableLetters.length === 26) {
-        return 'beast';
-    }
+  if (availableLetters.length === 26) {
+    return 'beast';
+  }
 
-    const wordList = await getWordList();
+  const wordList = await getWordList();
 
-    const step1 = filterOutBadLetters(wordList, currentState);
-    const step2 = filterOnlyWordsWithGuaranteedLetters(step1, currentState);
-    const step3 = filterOutGuessedWords(step2, gussedWords);
-    const step4 = filterOnlyAbsolutePosition(step3, FOR_SURE);
-    const step5 = filterOutWrongPositionLetters(step4, currentState, FOR_SURE);
+  const step1 = filterOutBadLetters(wordList, currentState);
+  const step2 = filterOnlyWordsWithGuaranteedLetters(step1, currentState);
+  const step3 = filterOutGuessedWords(step2, gussedWords);
+  const step4 = filterOnlyAbsolutePosition(step3, FOR_SURE);
+  const step5 = filterOutWrongPositionLetters(step4, currentState, FOR_SURE);
 
-    return step5[0];
-}
+  return step5[0];
+};
 
 const submitResults = (guess: string, results: string) => {
-    gussedWords.push(guess);
+  gussedWords.push(guess);
     
-    for (let i = 0; i < guess.length; i++) {
-        const letter = guess[i];
-        const result = results[i];
-        const state = letters.find(state => state.letter === letter);
-        if (!state) return
+  for (let i = 0; i < guess.length; i++) {
+    const letter = guess[i];
+    const result = results[i];
+    const state = letters.find(state => state.letter === letter);
+    if (!state) return;
 
-        state.checked = true;
-        if (result === 'y') {
-            state.contains = true;
-            FOR_SURE[i] = letter;
-            state.checkedPosition = state.checkedPosition ? [...state.checkedPosition, i] : [i];
-        } else if (result === 'm') {
-            state.contains = true;
-            state.checkedPosition = state.checkedPosition ? [...state.checkedPosition, i] : [i];
-        }
+    state.checked = true;
+    if (result === 'y') {
+      state.contains = true;
+      FOR_SURE[i] = letter;
+      state.checkedPosition = state.checkedPosition ? [...state.checkedPosition, i] : [i];
+    } else if (result === 'm') {
+      state.contains = true;
+      state.checkedPosition = state.checkedPosition ? [...state.checkedPosition, i] : [i];
     }
-}
+  }
+};
 
 const main = async () => {
-
-    for (let i = 0; i < 6; i++) {
-        const guess = await generateGuess(letters);
-        console.log(`Guess: ${guess}`);
-        const results: string = await new Promise(res => {
-            rl.question('What were the results? (y/m/n) ', res);
-        });
-        submitResults(guess, results);
-        if (results === 'yyyyy') {
-            console.log('nice :)')
-            break;
-        }
+  for (let i = 0; i < 6; i++) {
+    const guess = await generateGuess(letters);
+    console.log(`Guess: ${guess}`);
+    const results: string = await new Promise(res => {
+      rl.question('What were the results? (y/m/n) ', res);
+    });
+    submitResults(guess, results);
+    if (results === 'yyyyy') {
+      console.log('nice :)');
+      break;
     }
-
-}
+  }
+};
 
 main();
